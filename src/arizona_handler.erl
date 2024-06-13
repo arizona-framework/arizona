a1684b47e75cf1adec300195835bcb5db3c1f849
William Fank Thomé <willegp88@gmail.com>
2024-06-13 17:50:22 -0300

    refactor: remove example's extra slashes

--- src/arizona_handler.erl
+++ src/arizona_handler.erl
@@ -60,9 +60,9 @@ render(Macros0) ->
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{_@title}</title>
         <script src="assets/js/morphdom.min.js"></script>
         <script src="assets/js/arizona.js"></script>
