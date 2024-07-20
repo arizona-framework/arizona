0888da83b882ca0a0bc883d98226a5d66b9e708b
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-07-20 16:15:06 +0100

    Don't use tabs
    I don't know where that one came from

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-	{ files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
 ];
--- priv/static/assets/js/arizona.js
+++ priv/static/assets/js/arizona.js
@@ -2,118 +2,118 @@
 'use strict';
 
 globalThis['arizona'] = (() => {
-	// --------------------------------------------------------------------
-	// API function definitions
-	// --------------------------------------------------------------------
+  // --------------------------------------------------------------------
+  // API function definitions
+  // --------------------------------------------------------------------
 
-	function on(eventName, listener) {
-		listeners[eventName] = listeners[eventName] = [];
-		listeners[eventName].push(listener);
+  function on(eventName, listener) {
+    listeners[eventName] = listeners[eventName] = [];
+    listeners[eventName].push(listener);
 
-		logTable('on', eventName);
-	}
+    logTable('on', eventName);
+  }
 
-	function once(eventName, listener) {
-		const onceWrapper = (payload) => {
-			listener(payload);
-			off(eventName, onceWrapper);
-		};
-		on(eventName, onceWrapper);
+  function once(eventName, listener) {
+    const onceWrapper = (payload) => {
+      listener(payload);
+      off(eventName, onceWrapper);
+    };
+    on(eventName, onceWrapper);
 
-		logTable('once', eventName);
-	}
+    logTable('once', eventName);
+  }
 
-	function off(eventName, listener) {
-		listeners[eventName] = (listeners[eventName] || []).filter(
-			(needle) => needle !== listener,
-		);
+  function off(eventName, listener) {
+    listeners[eventName] = (listeners[eventName] || []).filter(
+      (needle) => needle !== listener,
+    );
 
-		logTable('off', eventName);
-	}
+    logTable('off', eventName);
+  }
 
-	function emit(eventName, payload) {
-		(listeners[eventName] || []).forEach((listener) => {
-			listener(payload);
-		});
-	}
+  function emit(eventName, payload) {
+    (listeners[eventName] || []).forEach((listener) => {
+      listener(payload);
+    });
+  }
 
-	function send(eventName, payload, listener) {
-		sendToWorker.bind(this)(eventName, payload, listener);
-	}
+  function send(eventName, payload, listener) {
+    sendToWorker.bind(this)(eventName, payload, listener);
+  }
 
-	function connect(listener) {
-		send('connect', { path: location.pathname }, listener);
-	}
+  function connect(listener) {
+    send('connect', { path: location.pathname }, listener);
+  }
 
-	// --------------------------------------------------------------------
-	// Private
-	// --------------------------------------------------------------------
+  // --------------------------------------------------------------------
+  // Private
+  // --------------------------------------------------------------------
 
-	const listeners = {};
-	const worker = new Worker('assets/js/arizona-worker.js');
+  const listeners = {};
+  const worker = new Worker('assets/js/arizona-worker.js');
 
-	function logTable(action, eventName) {
-		console.table({ action, eventName, listeners });
-	}
+  function logTable(action, eventName) {
+    console.table({ action, eventName, listeners });
+  }
 
-	function logDebug(scope, data) {
-		console.debug(`${scope}: ${data}`);
-	}
+  function logDebug(scope, data) {
+    console.debug(`${scope}: ${data}`);
+  }
 
-	function logError(scope, data) {
-		console.error(`${scope}: ${data}`);
-	}
+  function logError(scope, data) {
+    console.error(`${scope}: ${data}`);
+  }
 
-	function sendToWorker(eventName, payload, listener) {
-		let target;
-		if (this instanceof HTMLElement) {
-			target = document.querySelector(this.getAttribute('arizona-target'));
-		}
-		target = target?.getAttribute('arizona-id') || '[0]';
+  function sendToWorker(eventName, payload, listener) {
+    let target;
+    if (this instanceof HTMLElement) {
+      target = document.querySelector(this.getAttribute('arizona-target'));
+    }
+    target = target?.getAttribute('arizona-id') || '[0]';
 
-		listener && once(eventName, listener);
+    listener && once(eventName, listener);
 
-		worker.postMessage({ eventName, payload, target });
-	}
+    worker.postMessage({ eventName, payload, target });
+  }
 
-	function applyPatch(elem, html) {
-		morphdom(elem, html, {
-			onBeforeElUpdated: (from, to) => !from.isEqualNode(to),
-		});
-	}
+  function applyPatch(elem, html) {
+    morphdom(elem, html, {
+      onBeforeElUpdated: (from, to) => !from.isEqualNode(to),
+    });
+  }
 
-	// --------------------------------------------------------------------
-	// Namespace initialization
-	// --------------------------------------------------------------------
+  // --------------------------------------------------------------------
+  // Namespace initialization
+  // --------------------------------------------------------------------
 
-	worker.addEventListener('message', (event) => {
-		// `event` is:
-		// {
-		//   data: {
-		//     eventName: <string>,
-		//     payload: {
-		//       target: <array>,
-		//       html: <string>
-		//     },
-		//   }
-		// }
-		logDebug('[WebWorker] message', event.data);
+  worker.addEventListener('message', (event) => {
+    // `event` is:
+    // {
+    //   data: {
+    //     eventName: <string>,
+    //     payload: {
+    //       target: <array>,
+    //       html: <string>
+    //     },
+    //   }
+    // }
+    logDebug('[WebWorker] message', event.data);
 
-		const { eventName, payload } = event.data;
-		if (eventName === 'patch') {
-			const { target, html } = payload;
-			const elem = document.querySelector(
-				`[arizona-id="${JSON.stringify(target)}"]`,
-			);
-			applyPatch(elem, html);
-		}
+    const { eventName, payload } = event.data;
+    if (eventName === 'patch') {
+      const { target, html } = payload;
+      const elem = document.querySelector(
+        `[arizona-id="${JSON.stringify(target)}"]`,
+      );
+      applyPatch(elem, html);
+    }
 
-		emit(eventName, payload);
-	});
+    emit(eventName, payload);
+  });
 
-	worker.addEventListener('error', (event) =>
-		logError('[WebWorker] error', event),
-	);
+  worker.addEventListener('error', (event) =>
+    logError('[WebWorker] error', event),
+  );
 
-	return { on, once, off, send, connect };
+  return { on, once, off, send, connect };
 })();
