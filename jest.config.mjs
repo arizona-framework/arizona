2dc09e4248f15e48c0bda093bf7a10f5347eddd3
William Fank Thomé <williamthome@hotmail.com>
2025-04-05 15:33:55 -0300

    Setup JS coverage (#315)

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,13 @@
 export default {
+  testRegex: 'assets/.*\\.test\\.js$',
+  testPathIgnorePatterns: ['_build/', '.github'],
+  modulePathIgnorePatterns: ['_build/', '.github'],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['_build/', '.github'],
+  coverageDirectory: '_build/test/cover/js',
+  coverageProvider: 'v8',
   transform: {
     '^.+\\.(js|mjs)$': 'babel-jest',
   },
-  moduleFileExtensions: ['js', 'mjs'],
+  moduleFileExtensions: ['js', 'mjs', 'json'],
 };
