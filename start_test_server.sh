1cc89338e82cb60c8939276511c53e5a3b97e193
williamthome <williamthome@hotmail.com>
2025-07-09 19:51:53 -0300

    Fix E2E test server startup for CI environment
    Replace rebar3 shell with direct erl command for non-interactive CI execution.
    This resolves server termination issues in GitHub Actions by using erl -eval
    with proper dependency paths including test modules.

--- start_test_server.sh
+++ start_test_server.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-exec rebar3 as test shell --apps arizona --eval "
-application:ensure_all_started(arizona),
+# Use erl directly for CI environment instead of rebar3 shell
+exec erl \
+    -pa "_build/test/lib/arizona/ebin" \
+    -pa "_build/test/lib/arizona/test" \
+    -pa "_build/test/lib/cowboy/ebin" \
+    -pa "_build/test/lib/cowlib/ebin" \
+    -pa "_build/test/lib/ranch/ebin" \
+    -noshell -eval "
+{ok, _} = application:ensure_all_started(arizona),
 Routes = [
-    {live, ~\"/test/counter\", arizona_counter_live},
-    {live, ~\"/test/todo\", arizona_todo_app_live},
-    {live_websocket, ~\"/live/websocket\"},
-    {static, ~\"/assets\", {priv_dir, arizona, ~\"static/assets\"}}
+    {live, <<\"/test/counter\">>, arizona_counter_live},
+    {live, <<\"/test/todo\">>, arizona_todo_app_live},
+    {live_websocket, <<\"/live/websocket\">>},
+    {static, <<\"/assets\">>, {priv_dir, arizona, <<\"static/assets\">>}}
 ],
 case arizona_server:start(#{port => 8080, routes => Routes}) of
     {ok, _} -> 
