d8437b2b4c57cc6aee7097d66e11938436b21282
williamthome <williamthome@hotmail.com>
2025-07-16 18:35:58 -0300

    Refactor arizona template system to use function callbacks
    - Replace tuple-based template patterns with function callbacks
    - Move call_stateful/3 from resolver to arizona_template module
    - Move call_stateless/4 from resolver to arizona_template module
    - Simplify resolver logic to handle generic function callbacks
    - render_stateful/2 now returns fun(Socket) -> {Template, Socket1}
    - render_stateless/3 now returns fun(Socket) -> {Template, Socket1}
    - Remove complex tuple pattern matching from resolve_dynamic_callbacks
    - Cleaner separation of concerns between template creation and resolution

--- src/arizona_resolver.erl
+++ src/arizona_resolver.erl
@@ -32,52 +32,17 @@ resolve_template_dynamic(Template, Socket) ->
 resolve_dynamic_callbacks([], _DynamicTuple, Socket) ->
     {[], Socket};
 resolve_dynamic_callbacks([Index | T], DynamicTuple, Socket) ->
-    case apply(element(Index, DynamicTuple), []) of
-        {'$arizona_render_stateful_template', Mod, Bindings} ->
-            {StatefulTemplate, UpdatedSocket} = call_stateful(Mod, Bindings, Socket),
-            {Html, StatefulSocket} = resolve_template(StatefulTemplate, UpdatedSocket),
+    DynamicCallback = element(Index, DynamicTuple),
+    case DynamicCallback() of
+        Callback when is_function(Callback, 1) ->
+            {Template, TemplateSocket} = Callback(Socket),
+            {Html, StatefulSocket} = resolve_template(Template, TemplateSocket),
             {RestHtml, FinalSocket} = resolve_dynamic_callbacks(T, DynamicTuple, StatefulSocket),
-            {[Html | RestHtml], FinalSocket};
-        {'$arizona_render_stateless_template', Mod, Fun, Bindings} ->
-            StatelessTemplate = arizona_stateless:call_render_callback(Mod, Fun, Bindings),
-            TempSocket = arizona_socket:with_temp_bindings(Bindings, Socket),
-            {Html, StatelessSocket} = resolve_template(StatelessTemplate, TempSocket),
-            {RestHtml, FinalSocket} = resolve_dynamic_callbacks(T, DynamicTuple, StatelessSocket),
             {[Html | RestHtml], FinalSocket};
         Result ->
             {Html, NewSocket} = arizona_html:to_html(Result, Socket),
             {RestHtml, FinalSocket} = resolve_dynamic_callbacks(T, DynamicTuple, NewSocket),
             {[Html | RestHtml], FinalSocket}
-    end.
-
--spec call_stateful(Mod, Bindings, Socket) -> {Template, Socket1} when
-    Mod :: module(),
-    Bindings :: arizona_socket:bindings(),
-    Socket :: arizona_socket:socket(),
-    Template :: arizona_template:template(),
-    Socket1 :: arizona_socket:socket().
-call_stateful(Mod, Bindings, Socket) ->
-    Id = maps:get(id, Bindings),
-    case arizona_socket:find_stateful_state(Id, Socket) of
-        {ok, State} ->
-            %% Apply new bindings to existing state before checking remount
-            UpdatedState = arizona_stateful:put_bindings(Bindings, State),
-            %% Update socket with new state and call render callback (which handles diffing)
-            Socket1 = arizona_socket:put_stateful_state(UpdatedState, Socket),
-            UpdatedBindings = arizona_stateful:get_bindings(UpdatedState),
-            Template = arizona_stateful:call_render_callback(Mod, UpdatedBindings),
-            {Template, Socket1};
-        error ->
-            State = arizona_stateful:new(Id, Mod, Bindings),
-            Socket1 = arizona_socket:put_stateful_state(State, Socket),
-            %% Call mount callback for new components
-            Socket2 = arizona_stateful:call_mount_callback(Mod, Socket1),
-            %% Call the component's render callback which handles
-            %% rendering and returns updated socket
-            MountedState = arizona_socket:get_stateful_state(Id, Socket2),
-            MountedBindings = arizona_stateful:get_bindings(MountedState),
-            Template = arizona_stateful:call_render_callback(Mod, MountedBindings),
-            {Template, Socket2}
     end.
 
 %% Zip static and dynamic parts for list item
