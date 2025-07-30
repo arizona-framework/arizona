9a71d1cc9d4972296fee580fd3478655d26e1402
williamthome <williamthome@hotmail.com>
2025-07-30 10:12:29 -0300

    Recover E2E test files from commit 87dffdb8fe2b
    Recovered arizona_counter_layout.erl, arizona_counter_live.erl,
    arizona_todo_app_layout.erl, and arizona_todo_app_live.erl from
    commit 7e561ef before they were deleted.
    
    Note: These files use the old arizona_socket API and will need
    to be updated to work with the current arizona_view API.

---
+++ test/support/e2e/arizona_counter_layout.erl
@@ -0,0 +1,23 @@
+-module(arizona_counter_layout).
+%-compile({parse_transform, arizona_parse_transformform}).
+-arizona_parse_transformform([render/1]).
+-export([render/1]).
+
+%% Layout for the counter test component
+render(Socket) ->
+    arizona_html:render_stateless(~"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Arizona Test Counter</title>
+    </head>
+    <body>
+        {arizona_html:render_slot(main_content, Socket)}
+        <script type="module">
+            import Arizona from '/assets/js/arizona.min.js';
+            globalThis.arizona = new Arizona();
+            arizona.connect();
+        </script>
+    </body>
+    </html>
+    """, Socket).
---
+++ test/support/e2e/arizona_counter_live.erl
@@ -0,0 +1,34 @@
+-module(arizona_counter_live).
+-behaviour(arizona_live).
+-compile([nowarn_unused_vars]).
+%-compile({parse_transform, arizona_parse_transformform}).
+-arizona_parse_transformform([render/1]).
+
+-export([mount/2, render/1, handle_event/3]).
+
+% Simple counter component for E2E testing
+mount(_Params, Socket) ->
+    Socket1 = arizona_socket:put_binding(count, 0, Socket),
+    arizona_socket:set_layout({arizona_counter_layout, render, main_content}, Socket1).
+
+render(Socket) ->
+    Count = arizona_socket:get_binding(count, Socket),
+    arizona_html:render_live(~"""
+    <div id="root">
+        <h1>Counter: <span data-testid="count">{Count}</span></h1>
+        <button data-testid="increment" onclick="arizona.sendEvent('increment')">+</button>
+        <button data-testid="decrement" onclick="arizona.sendEvent('decrement')">-</button>
+        <button data-testid="reset" onclick="arizona.sendEvent('reset')">Reset</button>
+    </div>
+    """, Socket).
+
+handle_event(~"increment", _Payload, Socket) ->
+    Count = arizona_socket:get_binding(count, Socket),
+    {noreply, arizona_socket:put_binding(count, Count + 1, Socket)};
+handle_event(~"decrement", _Payload, Socket) ->
+    Count = arizona_socket:get_binding(count, Socket),
+    {noreply, arizona_socket:put_binding(count, Count - 1, Socket)};
+handle_event(~"reset", _Payload, Socket) ->
+    {noreply, arizona_socket:put_binding(count, 0, Socket)};
+handle_event(_Event, _Payload, Socket) ->
+    {noreply, Socket}.
