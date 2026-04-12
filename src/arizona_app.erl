544712b222a9b6f68ac4a654ddc8fac87b1ca795
williamthome <williamthome@hotmail.com>
2026-04-12 14:55:26 -0300

    Add moduledoc and reorganize arizona_app

--- src/arizona_app.erl
+++ src/arizona_app.erl
@@ -1,9 +1,30 @@
 -module(arizona_app).
+-moduledoc """
+OTP application entry point. Starts `arizona_sup` on boot.
+""".
 -behaviour(application).
--export([start/2, stop/1]).
 
+%% --------------------------------------------------------------------
+%% application callback exports
+%% --------------------------------------------------------------------
+
+-export([start/2]).
+-export([stop/1]).
+
+%% --------------------------------------------------------------------
+%% application Callbacks
+%% --------------------------------------------------------------------
+
+-spec start(StartType, StartArgs) -> StartRet when
+    StartType :: application:start_type(),
+    StartArgs :: term(),
+    StartRet :: {ok, Pid} | {error, ErrReason},
+    Pid :: pid(),
+    ErrReason :: term().
 start(_Type, _Args) ->
     arizona_sup:start_link().
 
+-spec stop(State) -> ok when
+    State :: term().
 stop(_State) ->
     ok.
