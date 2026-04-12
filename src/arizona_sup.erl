0645662d04acc6ad2165288babe94ff8bbe792f9
williamthome <williamthome@hotmail.com>
2026-04-12 15:03:23 -0300

    Add moduledoc and reorganize arizona_sup

--- src/arizona_sup.erl
+++ src/arizona_sup.erl
@@ -1,10 +1,45 @@
 -module(arizona_sup).
--behaviour(supervisor).
--export([start_link/0, init/1]).
+-moduledoc """
+Top-level supervisor for the Arizona application.
 
+Currently supervises only `arizona_pubsub` (the `pg`-based pubsub
+scope). Live processes are not managed here -- they're started ad hoc
+by the transport layer (`arizona_socket:init/3`) and linked to the
+calling WebSocket process so they share its lifetime.
+""".
+-behaviour(supervisor).
+
+%% --------------------------------------------------------------------
+%% API function exports
+%% --------------------------------------------------------------------
+
+-export([start_link/0]).
+
+%% --------------------------------------------------------------------
+%% supervisor callback exports
+%% --------------------------------------------------------------------
+
+-export([init/1]).
+
+%% --------------------------------------------------------------------
+%% API Functions
+%% --------------------------------------------------------------------
+
+-doc """
+Starts the supervisor under the well-known name `arizona_sup`.
+""".
+-spec start_link() -> supervisor:startlink_ret().
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+%% --------------------------------------------------------------------
+%% supervisor Callbacks
+%% --------------------------------------------------------------------
+
+-spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} when
+    Args :: list(),
+    SupFlags :: supervisor:sup_flags(),
+    ChildSpec :: supervisor:child_spec().
 init([]) ->
     {ok,
         {#{strategy => one_for_one}, [
