8820ad6b16f37c0a2ae341c162975dd540df4c37
williamthome <williamthome@hotmail.com>
2026-08-30 17:14:32 -0300

    Ignore xref on the transform constructors

--- src/arizona_middleware.erl
+++ src/arizona_middleware.erl
@@ -99,6 +99,19 @@ builder.
 %% Ignore xref warnings
 %% --------------------------------------------------------------------
 
+%% HTTP response transform constructors: public API for route declarations;
+%% no internal callers by design (the router matches the tagged term).
+-ignore_xref([etag/0]).
+-ignore_xref([compress/0]).
+-ignore_xref([cors/1]).
+-ignore_xref([security_headers/0]).
+-ignore_xref([security_headers/1]).
+-ignore_xref([http_transform/1]).
+
+%% --------------------------------------------------------------------
+%% Ignore xref warnings
+%% --------------------------------------------------------------------
+
 -ignore_xref([extract/1]).
 -ignore_xref([put_request/2]).
 -ignore_xref([fetch_flash/2]).
